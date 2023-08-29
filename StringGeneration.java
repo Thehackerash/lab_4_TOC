ee4a78f8c3fa2b6bc027af9609561e46a4788729
Thehackerash <112200151+Thehackerash@users.noreply.github.com>
2023-08-29 16:21:06 +0530

    Update StringGeneration.java

--- StringGeneration.java
+++ StringGeneration.java
@@ -1,39 +1,26 @@
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class StringGeneration {
-    public static void generateStrings(String characters, int minLength, int maxLength, String current, List<String> result) {
-        if (minLength <= 0) {
-            result.add(current);
-        }
-        if (minLength <= maxLength) {
-            for (char c : characters.toCharArray()) {
-                generateStrings(characters, minLength - 1, maxLength, current + c, result);
-            }
+public class RandomStringGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first length:");
+        int firstLength = sc.nextInt();
+        System.out.println("Enter the second length:");
+        int secondLength = sc.nextInt();
+        String[] arr = {"0", "1"};
+        for (int i = firstLength; i <= secondLength; i++) {
+            generateStrings(arr, "", arr.length, i);
         }
     }
-    public static void main(String[] args) {
-        String characters = "P*";
-        int minLength, maxLength;
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter minimum length: ");
-        minLength = scanner.nextInt();
-
-        System.out.print("Enter maximum length: ");
-        maxLength = scanner.nextInt();
-
-        scanner.close();
-
-        List<String> generatedStrings = new ArrayList<>();
-        generateStrings(characters, minLength, maxLength, "", generatedStrings);
-
-        System.out.println("Generated strings:");
-        for (String s : generatedStrings) {
-            System.out.println(s);
+    public static void generateStrings(String[] arr, String currentString, int n, int length) {
+        if (length == 0) {
+            System.out.println(currentString);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            String newString = currentString + arr[i];
+            generateStrings(arr, newString, n, length - 1);
         }
     }
 }
